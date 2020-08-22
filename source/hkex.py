e3f42d1dddee9797c86d576453b5d3fd9fac5dbe
hackeris <hackeris@qq.com>
2020-08-22 12:11:46 +0800

    fix hkgr

--- source/hkex.py
+++ source/hkex.py
@@ -10,7 +10,7 @@ def get_north_bond_top(date):
     url = 'https://www.hkex.com.hk/chi/csm/DailyStat/data_tab_daily_%sc.js' % date
     response = request.urlopen(url)
     body = response.read().decode('utf-8')
-    data = json.loads(body.split('=')[1])
+    data = json.loads(body.replace('\\', '').split('=')[1])
     sse = data[0]['content'][1]['table']['tr']
     sze = data[2]['content'][1]['table']['tr']
     return [fill_code(item['td'][0][1]) for item in sse + sze]
